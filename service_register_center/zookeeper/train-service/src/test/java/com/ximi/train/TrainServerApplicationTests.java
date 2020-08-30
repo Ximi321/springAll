f1de58f663e732d0c2940a5c753d6321541f3f4d
Ximi <liu.qing@belle.com.cn>
2020-08-30 10:22:54 +0800

    zookeeper

---
+++ service_register_center/zookeeper/train-service/src/test/java/com/ximi/train/TrainServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ximi.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
