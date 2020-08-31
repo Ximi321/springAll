dfa66b656bba5fbe645e696b1dcfb1ba5799b1af
Ximi <liu.qing@belle.com.cn>
2020-08-31 14:02:09 +0800

    nacos

---
+++ service_register_center/nacos/train-customer/src/main/java/com/ximi/train/customer/TrainCustomerApplication.java
@@ -0,0 +1,26 @@
+package com.ximi.train.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class TrainCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainCustomerApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
---
+++ service_register_center/nacos/train-customer/src/main/java/com/ximi/train/customer/service/StudentService.java
@@ -0,0 +1,16 @@
+package com.ximi.train.customer.service;
+
+import com.ximi.train.customer.model.Student;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "train-service",contextId = "student",path = "student")
+public interface StudentService {
+
+    @GetMapping(path = "byName")
+    public List<Student> getByName(@RequestParam("name") String name);
+
+}
